f7a11a6134385b931703f099b454320a2d525275
ksmai <kasingmai@gmail.com>
2020-09-20 17:12:16 +0800

    Provide script to run jekyll commands in docker

---
+++ jekyll.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.8 jekyll "$@"
--- serve.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker run --rm --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:3.8 jekyll serve
